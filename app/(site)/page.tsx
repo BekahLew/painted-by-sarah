65ceeb94a1ba494d4fa03a5c08eda20ade5c342f
Becky Springer <beckyspringer@Mac.home>
2025-01-23 19:11:58 -0800

    import fixes

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,10 @@
 
-import { getProjects } from "@/sanity/sanity-utils";
-import Image from "next/image";
-import Link from "next/link";
+// import { getProjects } from "@/sanity/sanity-utils";
+// import Image from "next/image";
+// import Link from "next/link";
 
 export default async function Home() {
-  const projects = await getProjects();
+  // const projects = await getProjects();
 
   return (
     <div>
